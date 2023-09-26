9575c0e0ceba3f915e21e22d10fc7d3b30f6f1c7
dannybowmann <127047876+dannybowmann@users.noreply.github.com>
2023-09-25 22:33:32 -0400

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+let i = 0
+  while (i <= times) {
+    return word
+    i++
+  }
 };
-
+function('hey', 5)
 // Do not edit below this line
 module.exports = repeatString;
